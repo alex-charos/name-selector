12f3c83b06b47689e513d22c91f7db7283375971
Alex <realtowz@yahoo.com>
2017-08-20 21:50:36 +0300

    minor fixes

--- routes/names.js
+++ routes/names.js
@@ -4,20 +4,28 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	var names = [];
-                names.push({"name":"ΑΡΙΑΔΝΗ", "url":"https://el.wikipedia.org/wiki/Αριάδνη"});
-                names.push({"name":"ΙΟΛΗ", "url":"https://el.wikipedia.org/wiki/Ιόλη"});
-                names.push({"name":"ΙΟΚΑΣΤΗ", "url":"N/A"});
-                names.push({"name":"ΕΜΜΕΛΕΙΑ", "url":"N/A"});
-                names.push({"name":"ΑΝΤΙΓΟΝΗ", "url":"N/A"});
-                names.push({"name":"ΙΣΜΗΝΗ", "url":"N/A"});
-                names.push({"name":"ΔΩΡΟΘΕΑ", "url":"N/A"});
-                names.push({"name":"ΗΛΕΚΤΡΑ", "url":"N/A"});
-                names.push({"name":"ΜΑΛΕΝΑ", "url":"N/A"});
-                names.push({"name":"ΙΩΑΝΝΑ", "url":"N/A"});
-                names.push({"name":"ΜΑΡΙΑ", "url":"N/A"});
-                 names.push({"name":"ΝΕΦΕΛΗ", "url":"N/A"});
-                 names.push({"name":"ΚΑΛΛΙΣΤΩ", "url":"N/A"});
-  res.send(names);
+        names.push({"name":"ΑΡΙΑΔΝΗ", "url":"https://el.wikipedia.org/wiki/Αριάδνη"});
+        // names.push({"name":"ΙΟΛΗ", "url":"https://el.wikipedia.org/wiki/Ιόλη"});
+        // names.push({"name":"ΙΟΚΑΣΤΗ", "url":"N/A"});
+        // names.push({"name":"ΕΜΜΕΛΕΙΑ", "url":"N/A"});
+        // names.push({"name":"ΑΝΤΙΓΟΝΗ", "url":"N/A"});
+        // names.push({"name":"ΙΣΜΗΝΗ", "url":"N/A"});
+        // names.push({"name":"ΔΩΡΟΘΕΑ", "url":"N/A"});
+        // names.push({"name":"ΗΛΕΚΤΡΑ", "url":"N/A"});
+        // names.push({"name":"ΜΑΛΕΝΑ", "url":"N/A"});
+        // names.push({"name":"ΙΩΑΝΝΑ", "url":"N/A"});
+        // names.push({"name":"ΜΑΡΙΑ", "url":"N/A"});
+        // names.push({"name":"ΝΕΦΕΛΗ", "url":"N/A"});
+        // names.push({"name":"ΚΑΛΛΙΣΤΩ", "url":"N/A"});
+
+        var parents = [];
+        parents.push({"name": "Alex"});
+        parents.push({"name": "Christina"});
+
+
+        var obj = {"names" : names, "parents": parents}
+
+  res.send(obj);
 });
 
 module.exports = router;
